ee09d6d7c33d88b38dcc563e97759c5e75acf5e9
jevgenij3141 <jevgenij.sreda@itpc.vu.lt>
2019-02-27 14:11:02 +0200

    First step done

--- main.cpp
+++ main.cpp
@@ -1,59 +1,100 @@
-// Включаем стандартные заголовки
-#include <stdio.h>
-#include <stdlib.h>
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include <SDL2/SDL.h>
 
-#include<X11/X.h>
-#include<X11/Xlib.h>
+///usr/lib/x86_64-linux-gnu/libGLU.so - not included
 
-// Включаем GLEW. Всегда включайте его ДО gl.h и glfw.h
-#include "GL/glew.h"
+// use border state as proxy for fullscreenedness
+SDL_Rect ToggleFakeFullscreen( SDL_Window* window, const SDL_Rect& oldBounds )
+{
+    if( SDL_GetWindowFlags( window ) & SDL_WINDOW_BORDERLESS )
+    {
+        SDL_SetWindowBordered( window, SDL_TRUE );
+        SDL_SetWindowSize( window, oldBounds.w, oldBounds.h );
+        SDL_SetWindowPosition( window, oldBounds.x, oldBounds.y );
+        return oldBounds;
+    }
+    else
+    {
+        SDL_Rect curBounds;
+        SDL_GetWindowPosition( window, &curBounds.x, &curBounds.y );
+        SDL_GetWindowSize( window, &curBounds.w, &curBounds.h );
 
-// Включаем GLFW
-#include "GLFW/glfw3.h"
+        int idx = SDL_GetWindowDisplayIndex( window );
+        SDL_Rect bounds;
+        SDL_GetDisplayBounds( idx, &bounds );
+        SDL_SetWindowBordered( window, SDL_FALSE );
+        SDL_SetWindowPosition( window, bounds.x, bounds.y );
+        SDL_SetWindowSize( window, bounds.w, bounds.h );
 
-#include<GL/gl.h>
-#include<GL/glx.h>
-#include<GL/glu.h>
-
-
-Display                 *dpy;
-Window                  root;
-GLint                   att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
-XVisualInfo             *vi;
-Colormap                cmap;
-XSetWindowAttributes    swa;
-Window                  win;
-GLXContext              glc;
-XWindowAttributes       gwa;
-XEvent                  xev;
-
-void DrawAQuad() {
- glClearColor(1.0, 1.0, 1.0, 1.0);
- glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
- glMatrixMode(GL_PROJECTION);
- glLoadIdentity();
- glOrtho(-1., 1., -1., 1., 1., 20.);
-
- glMatrixMode(GL_MODELVIEW);
- glLoadIdentity();
- gluLookAt(0., 0., 10., 0., 0., 0., 0., 1., 0.);
-
- glBegin(GL_QUADS);
-  glColor3f(1., 0., 0.); glVertex3f(-.75, -.75, 0.);
-  glColor3f(0., 1., 0.); glVertex3f( .75, -.75, 0.);
-  glColor3f(0., 0., 1.); glVertex3f( .75,  .75, 0.);
-  glColor3f(1., 1., 0.); glVertex3f(-.75,  .75, 0.);
- glEnd();
+        return curBounds;
+    }
 }
 
-int main(){
-    // Инициализируем GLFW
-    /*if( !glfwInit() )
-    {
-        fprintf( stderr, "Ошибка при инициализации GLFWn" );
+int main( int argc, char **argv )
+{
+    if( SDL_Init( SDL_INIT_EVERYTHING ) < 0 )
         return -1;
-    }*/
 
-   return 0;
+    SDL_Window* window = SDL_CreateWindow
+        (
+        "Test",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        640, 480,
+        SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL
+        );
+    if( NULL == window )
+        return -1;
+
+    SDL_GLContext ctx = SDL_GL_CreateContext( window );
+    if( GLEW_OK != glewInit() )
+        return -1;
+
+    SDL_Rect curBounds;
+
+    bool running = true;
+    while( running )
+    {
+        SDL_Event ev;
+        while( SDL_WaitEventTimeout( &ev, 16 ) )
+        {
+            if( ev.type == SDL_QUIT )
+                running = false;
+            if( ev.type == SDL_KEYUP &&
+                ev.key.keysym.sym == SDLK_ESCAPE )
+                running = false;
+
+            if( ev.type == SDL_KEYUP &&
+                ev.key.keysym.sym == SDLK_f )
+                curBounds = ToggleFakeFullscreen( window, curBounds );
+        }
+
+        int w, h;
+        SDL_GetWindowSize( window, &w, &h );
+        glViewport( 0, 0, w, h );
+
+        glClearColor( 0, 0, 0, 1 );
+        glClear( GL_COLOR_BUFFER_BIT );
+
+        glMatrixMode( GL_PROJECTION );
+        glLoadIdentity();
+
+        glMatrixMode( GL_MODELVIEW );
+        glLoadIdentity();
+
+        glColor3ub( 255, 0, 0 );
+        glBegin( GL_TRIANGLES );
+        glVertex2i( -1, -1 );
+        glVertex2i(  1, -1 );
+        glVertex2i(  0,  1 );
+        glEnd();
+
+        SDL_GL_SwapWindow( window );
+    }
+
+    SDL_GL_DeleteContext( ctx );
+    SDL_DestroyWindow( window );
+    SDL_Quit();
+
+    return 0;
 }
